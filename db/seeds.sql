29334faf09fe80ddf2ac0e5ce74465b6ec1ac98a
Naeem Ali <alinaeem4321@gmail.com>
2018-06-01 19:31:09 -0500

    added all project files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tequila Lime Burger', true);
